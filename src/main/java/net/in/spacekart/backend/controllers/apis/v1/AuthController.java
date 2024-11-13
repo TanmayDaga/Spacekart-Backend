da42eae915beae7ee35f2f6ca0ec75b0dbea0101
Tanmay Daga <tanmay06daga@gmail.com>
2024-11-13 10:49:58 +0530

    changed logoout endpoint

--- src/main/java/net/in/spacekart/backend/controllers/apis/v1/AuthController.java
+++ src/main/java/net/in/spacekart/backend/controllers/apis/v1/AuthController.java
@@ -137,7 +137,7 @@ public class AuthController {
     }
 
     @PreAuthorize("hasRole('USER')")
-    @PostMapping(value =  "api/v1/logout")
+    @PostMapping(value =  "api/v1/logoutMe")
     public ResponseEntity<?> logout(HttpServletRequest request, HttpServletResponse response) {
         try {
             // Clear the security context
